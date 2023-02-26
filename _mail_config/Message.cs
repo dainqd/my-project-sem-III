2815e0027afdabf0efe145f893bbe043ab31f196
dainq <97323194+dainqd@users.noreply.github.com>
2023-02-27 01:25:35 +0700

    Test send mail ok.

---
+++ _mail_config/Message.cs
@@ -0,0 +1,19 @@
+using MimeKit;
+
+namespace myProject._mail_config;
+
+public class Message
+{
+    public List<MailboxAddress> To { get; set; }
+    public string Subject { get; set; }
+    public string Content { get; set; }
+
+    public Message(IEnumerable<string> to, string subject, string content)
+    {
+        To = new List<MailboxAddress>();
+
+        To.AddRange(to.Select(x => MailboxAddress.Parse(x)));
+        Subject = subject;
+        Content = content;        
+    }
+}
