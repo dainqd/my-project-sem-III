334453d208ce86f6e4789920c966e77102ae2b11
unknown <ngodaix5tp@gmail.com>
2023-03-07 17:25:28 +0700

    Create dtos feebacks.

---
+++ Dtos/Feedback/CreateFeedbackRequest.cs
@@ -0,0 +1,11 @@
+﻿using myProject.Utils.Enums;
+
+namespace myProject.Dtos.Feedback;
+
+public class CreateFeedbackRequest
+{
+    public string fullname { get; set; }
+    public string message { get; set; }
+    public string thumbnail { get; set; }
+    public Enums.FeedbackStatus status =  Enums.FeedbackStatus.PENDING;
+}
